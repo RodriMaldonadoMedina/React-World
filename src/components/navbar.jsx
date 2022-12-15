31cce9d5b25fe8f8cfda6405552253bb69f1654b
Rodrigo Hernan Maldonado Medina <maldonadomedinarodrigo@gmail.com>
2022-12-15 15:33:45 +0100

    empezando desde cero

---
+++ src/components/navbar.jsx
@@ -0,0 +1,17 @@
+import { BsMoon } from "react-icons/bs";
+import "../styles/navbar.css"
+
+const Navbar = () => {
+    return (
+        <nav className="navbar border-bottom border-3 shadow">
+            <div className="container-fluid d-flex justify-content-between mx-5">
+                <h2>Where in the world?</h2>
+                <button className="darkMode">
+                    <BsMoon/> Dark Mode
+                </button>
+            </div>
+        </nav>
+      );
+}
+ 
+export default Navbar;
