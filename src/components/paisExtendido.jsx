7f84daf6fc41414e00f2f5be67150af6faf60456
Rodrigo Hernan Maldonado Medina <maldonadomedinarodrigo@gmail.com>
2022-12-26 20:30:06 +0100

    casi listo, falta el dark mode

--- src/components/paisExtendido.jsx
+++ src/components/paisExtendido.jsx
@@ -9,7 +9,6 @@ const PaisExtendido = () => {
 
     useEffect(()=>{
         paisInfo(nombrePais);
-        
     },[])
 
     const paisInfo = async (nombre) => {
@@ -17,36 +16,43 @@ const PaisExtendido = () => {
             const resp = await fetch("https://restcountries.com/v3.1/name/" + nombre);
             const data = await resp.json();
             setPais(data[0]);
+            setLanguage(Object.values(data[0].languages));
         }
         catch (error) {
             console.log(error);
         }
     }
 
-    if (pais.language){
-        setLanguage(Object.values(pais.languages));
-        console.log(language[0]);
-    }    
-
+    console.log(pais);
     return (
         pais ?
-            <div>
+            <div className="container-fluid">
                 <Link to="/" className="btn btn-primary my-4">Back</Link>
-                <div className="d-flex">
-                    <div className="flag-container">
-                        <img src={pais.flags?.png}/>
+                <div className="d-flex justify-content-evenly mt-5 pais">
+                    <div className="flag-container mt-4">
+                        <img src={pais.flags?.png} alt="Bandera" className="border border-2 shadow"/>
                     </div>
-                    <div >
-                        <h2>{nombrePais}</h2>
+                    <div className="info-container">
+                        <h2 className="mb-3">{nombrePais}</h2>
                         <div>
-                            <p className="">Native Name: {pais.name?.common}</p>
-                            <p className="">Population: {pais.population}</p>
-                            <p className="">Region: {pais.region}</p>
-                            <p className="">Sub Region: {pais.subregion}</p>
-                            <p className="">Capital: {pais.capital}</p>
-                            <p className="">Top Level Domain: {pais.tld}</p>
-                            {/* <p className="">Currencies: {pais.currencies}</p> */}
-                            {/* <p className="">Languages: {Object.values(pais.languages)[0]}</p> */}
+                            <p className="m-0"><b>Native Name: </b>{pais.name?.common}</p>
+                            <p className="m-0"><b>Population: </b>{pais.population}</p>
+                            <p className="m-0"><b>Sub Region: </b>{pais.subregion}</p>
+                            <p className="m-0"><b>Region: </b>{pais.region}</p>
+                            <p className="m-0"><b>Top Level Domain: </b>{pais.tld}</p>
+                            <p className="m-0"><b>Languages: </b>{language.join(", ")}</p>
+                        </div>
+                        <div className="d-flex">
+                            <p><b>Border Countries: </b></p>
+                            <div>
+                                {pais.borders ? 
+                                    pais.borders.map((p,i)=>
+                                        <div key={i} className="btn btn-outline-secondary mx-1 mt-1">{p}</div>)
+                                    :
+                                    null
+                                }
+                            </div>
+                            
                         </div>
                     </div>
                 </div>
