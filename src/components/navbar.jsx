2fcdb8299db238d4f684d89cf67d9e0ba21543f3
Rodrigo Hernan Maldonado Medina <maldonadomedinarodrigo@gmail.com>
2023-01-07 19:25:36 +0100

    agregado el switch de cambio de mode

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,21 @@
 import React from "react";
-import { BsMoon } from "react-icons/bs";
+import { BsMoonFill,BsSunFill } from "react-icons/bs";
 import "../styles/navbar.css"
+
+const darkModeToggle = () => {
+    let btn = document.querySelector(".switch");
+    document.body.classList.toggle("darkMode");
+    btn.classList.toggle("active");
+}
 
 const Navbar = () => {
     return (
         <nav className="navbar border-bottom border-3 shadow">
             <div className="container-fluid d-flex justify-content-between mx-5">
                 <h2>Where in the world?</h2>
-                <button className="darkMode">
-                    {/* proximo a realizar */}
-                    <BsMoon/> Dark Mode
+                <button className="switch" onClick={darkModeToggle}>
+                    <span><BsSunFill/></span>
+                    <span><BsMoonFill/></span>
                 </button>
             </div>
         </nav>
